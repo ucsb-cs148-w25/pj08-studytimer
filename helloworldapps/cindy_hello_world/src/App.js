2b4027350fe7cce061bbb176a746c615c66c05ac
Cindy Zhao <xintongzhao@ucsb.edu>
2025-01-17 15:18:27 -0800

    Create hello world demo with React

---
+++ helloworldapps/cindy_hello_world/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Hello World!
+        </h1>
+        <p>
+          Cindy Zhao CS 148
+        </p>
+      </header>
+      <footer>
+        Made for pj08-study-timer!
+      </footer>
+    </div>
+  );
+}
+
+export default App;
