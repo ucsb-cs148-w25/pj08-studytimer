a264f840ed75585c2bedcb2d1713e96353fc8745
Kevin Lee <kevinlee@169-231-184-104.wireless.ucsb.edu>
2025-01-28 13:43:37 -0800

    fixed bug with navbar scaling on mobile

--- frontend/timewise/src/components/Navbar.js
+++ frontend/timewise/src/components/Navbar.js
@@ -1,28 +1,38 @@
 import { Link } from 'react-router-dom';
 import "./NavbarStyles.css";
+import { useState } from 'react';
 
 function Navbar() {
-  return(
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  return (
     <nav>
-      <Link to="/">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
-          {/* Your existing SVG paths */}
-        </svg>
-        <img id="logo" src="favicon.svg" alt="timewise logo"/>
-        <span id="webapp-title">timewise</span>
-      </Link>
       <div>
-        <ul id="navbar">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/calendar">Calendar</Link></li>
-          <li><Link to="/task_manager">To-Do List</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/settings">Settings</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+        <Link to="/" className="logo-link">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
+            {/* Your existing SVG paths */}
+          </svg>
+          <img id="logo" src="favicon.svg" alt="timewise logo" />
+          <span id="webapp-title">timewise</span>
+        </Link>
       </div>
+      {/* Hamburger menu - hidden on larger screens */}
+      <button
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!isMenuOpen)}
+      >
+        ☰
+      </button>
+      <ul id="navbar" className={isMenuOpen ? "active" : ""}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calendar">Calendar</Link></li>
+        <li><Link to="/task_manager">To-Do List</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
