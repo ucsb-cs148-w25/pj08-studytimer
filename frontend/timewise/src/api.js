99364767f800e8e766ccf902589502a5ae3bd866
Kevin Lee <kevinlee@169-231-136-43.wireless.ucsb.edu>
2025-01-30 22:45:59 -0800

    added API URL for netlify

--- frontend/timewise/src/api.js
+++ frontend/timewise/src/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
+// Use localhost in development, Render in production
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  (window.location.hostname === "localhost"
+    ? "http://127.0.0.1:5000"
+    : "https://pj08-studytimer.onrender.com");
 
 export const fetchData = async () => {
   try {
