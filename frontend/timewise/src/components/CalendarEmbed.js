89b47a0fe73d2f52810010ea7ff38abd752b2ce2
Edwin Medrano <eduino@Edwins-MacBook-Pro.local>
2025-02-02 13:17:54 -0800

    rounded off calendar embed to look cleaner

--- frontend/timewise/src/components/CalendarEmbed.js
+++ frontend/timewise/src/components/CalendarEmbed.js
@@ -6,7 +6,7 @@ const CalendarEmbed = () => {
         <div className="calendar-container">
             <iframe 
                 src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&showNav=0&showTabs=0&showCalendars=0&showTz=0&showTitle=0&mode=WEEK&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%230B8043" 
-                style={{ border: "none", width: "97%", height: "95%", marginTop: "100px" }}
+                style={{ border: "none", width: "97%", height: "95%", marginTop: "100px", borderRadius: "10px" }}
                 frameborder="0" 
                 title="myCalendar">
             </iframe>
